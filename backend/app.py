dc38e6a3e8cdc701fc53c7508c783fd78e634489
KrinaModi <krinamodi318@gmail.com>
2026-03-12 11:34:11 +0530

    Updated login feature and UI

--- backend/app.py
+++ backend/app.py
@@ -10,7 +10,12 @@ from flask import Flask, render_template, request, redirect
 
 from utils.preprocessing import clean_text
 from utils.features import extract_features
+import joblib
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+MODEL_PATH = os.path.join(BASE_DIR, "model", "threat_model.pkl")
+
+model = joblib.load(MODEL_PATH)
 
 # ------------------- Flask App -------------------
 app = Flask(
@@ -18,6 +23,7 @@ app = Flask(
     template_folder="../frontend/templates",
     static_folder="../frontend/static"
 )
+
 
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -62,6 +68,9 @@ def login():
     return render_template("login.html", error="Invalid username or password")
 
 
+
+
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
@@ -95,11 +104,74 @@ def register():
 
     return render_template("register.html")
 
+@app.route("/forgot-password", methods=["GET", "POST"])
+def forgot_password():
+    print("FORGOT PASSWORD ROUTE HIT")
+
+    if request.method == "POST":
+        username = request.form["username"]
+        email = request.form["email"]
+        new_password = request.form["new_password"]
+
+        rows = []
+        updated = False
+
+        with open("backend/users.csv", newline="", encoding="utf-8") as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                if row["username"] == username and row["email"] == email:
+                    row["password"] = new_password
+                    updated = True
+                rows.append(row)
+
+        if not updated:
+            return render_template(
+                "forgot_password.html",
+                message="User not found or email mismatch"
+            )
+
+        with open("backend/users.csv", "w", newline="", encoding="utf-8") as file:
+            writer = csv.DictWriter(
+                file,
+                fieldnames=["username", "email", "password"]
+            )
+            writer.writeheader()
+            writer.writerows(rows)
+
+        return redirect("/login")
+
+    return render_template("forgot_password.html")
+
+
 
 # 3️⃣ DASHBOARD (THREAT DETECTION PAGE)
+
+
 @app.route("/dashboard")
 def dashboard():
     return render_template("index.html")
+
+
+@app.route("/analyze", methods=["POST"])
+def analyze():
+
+    user_input = request.form["input_data"]
+
+    # Extract features
+    features = extract_features(user_input)
+
+    # Convert to dataframe
+    df = pd.DataFrame([features])
+
+    # Predict
+    prediction = model.predict(df)[0]
+
+    if prediction == 1:
+        result = "⚠️ Suspicious / Phishing"
+    else:
+        result = "✅ Safe"
+
+    return render_template("index.html", result=result)
 
 
 
